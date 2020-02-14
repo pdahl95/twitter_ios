a867f69e2df5398c8ff355f8e26f8bd5f7e30fef
Pernille Dahl <pdahl@csumb.edu>
2020-02-13 16:28:52 -0800

    Part 1 of Twitter

---
+++ Twitter/TweetCell.swift
@@ -0,0 +1,29 @@
+//
+//  TweetCell.swift
+//  Twitter
+//
+//  Created by Pernille Dahl on 2/13/20.
+//  Copyright © 2020 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetCell: UITableViewCell {
+    
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var tweetLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
