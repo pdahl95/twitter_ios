65bb59b52eeee2d11dc873c87ceff88bc62417f8
Pernille Dahl <pdahl@csumb.edu>
2020-02-22 17:28:52 -0800

    added character counter and limit

--- Twitter/TweetViewController.swift
+++ Twitter/TweetViewController.swift
@@ -8,19 +8,26 @@
 
 import UIKit
 
-class TweetViewController: UIViewController, UITextViewDelegate {
+class TweetViewController: UIViewController, UITextViewDelegate, UITextFieldDelegate {
 
     @IBOutlet weak var tweetTextView: UITextView!
-    
+    @IBOutlet weak var charLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tweetTextView.text = "What's happening?"
         tweetTextView.textColor = UIColor.lightGray
         tweetTextView.delegate = self
-//        tweetTextView.becomeFirstResponder()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+          let newText = (textView.text as NSString).replacingCharacters(in: range, with: text)
+        let change = 140 - newText.count
+        self.charLabel.text = String(format: "%d / 140", change)
+         return newText.count < 140
+     }
+  
     
     func tapTextView(sender: UITapGestureRecognizer){
         tweetTextView.becomeFirstResponder()
@@ -32,7 +39,7 @@ class TweetViewController: UIViewController, UITextViewDelegate {
             tweetTextView.textColor = UIColor.black
         }
     }
-
+    
     
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
